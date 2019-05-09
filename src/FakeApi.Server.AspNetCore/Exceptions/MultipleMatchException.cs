966d904e964e981bd374a5787da5ed81361851c2
Jacob Mather <jmather@jmather.com>
2019-05-08 23:29:41 -0700

    Fix exception issues flagged in sonarqube

--- src/FakeApi.Server.AspNetCore/Exceptions/MultipleMatchException.cs
+++ src/FakeApi.Server.AspNetCore/Exceptions/MultipleMatchException.cs
@@ -1,12 +1,15 @@
 using System;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 using FakeApi.Server.AspNetCore.Models;
 using FakeApi.Server.AspNetCore.Services;
 
 namespace FakeApi.Server.AspNetCore.Exceptions
 {
+    [Serializable]
     public sealed class MultipleMatchException : Exception
     {
+        [NonSerialized]
         public const string MessageText = "Multiple possible matches were found.";
         
         public MultipleMatchException(List<FakeEndpoint> bestMatches, int bestMatchCount,
@@ -17,6 +20,10 @@ namespace FakeApi.Server.AspNetCore.Exceptions
             Data.Add("BestMatches", bestMatches);
             Data.Add("BestMatchCount", bestMatchCount);
             Data.Add("AllMatches", scoredEndpoints);
+        }
+
+        private MultipleMatchException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
         }
     }
 }
